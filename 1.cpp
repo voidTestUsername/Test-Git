8fde4ec0a4d2166349583cd84053ffb5d25af256
voidTestUsername <74876379+voidTestUsername@users.noreply.github.com>
2020-11-23 22:10:49 +0300

    Added new object

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,14 @@
 #include "iostream"
+#include "string"
 using namespace std;
 
 void main()
 {
 	int a = 2, b = 3;
 	cout << "a = " << a << ", b = " << b << ", a + b = " << a + b << "\n\n";
+
+	string str = "String for new commit";
+	cout << "Update: " << str << "\n\n";
 
 	system('pause');
 }
